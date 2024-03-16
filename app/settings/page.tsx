66b032b7e205bd4799549d11cde9e47dafdbac4a
Jason Chiang <chiangjason19@gmail.com>
2024-03-15 23:50:11 -0700

    Added active link styling when on pathname nav menu

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,5 +1,3 @@
 export default function Page() {
-  return (
-    <>Settings page</>
-  )
+  return <>Settings page</>
 }
--- components/home/nav-menu.tsx
+++ components/home/nav-menu.tsx
@@ -1,18 +1,17 @@
 'use client'
 
 import clsx from 'clsx'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 import { useMediaQuery } from '@/lib/utils'
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
-import Link from 'next/link'
 
 // Navigation Menu
 // Inside NavgiationMenuContent is where we'll have the isMobile conditional
@@ -20,24 +19,44 @@ import Link from 'next/link'
 // Menu will consist of: Home (just a link), Settings (a Link to Settings page...), a Create Post (make this a modal or post depending on screen)
 // Lets add what we are currently active on, so have CSS ready for that
 
+// This is same as :hover from '@components/ui/navigation-menu'
+
 export default function NavMenu() {
   const isMobile = useMediaQuery('(max-width: 767px)')
+  const pathname = usePathname()
+
+  function activeLinkStyling(name: string): string | null {
+    return pathname === name ? 'bg-accent text-accent-foreground' : null
+  }
+
   return (
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
           <Link href="/home" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <NavigationMenuLink
+              className={clsx(
+                navigationMenuTriggerStyle(),
+                activeLinkStyling('/home')
+              )}
+            >
               Home
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
-        <NavigationMenuItem className={clsx(navigationMenuTriggerStyle(), "px-2")}>
+        <NavigationMenuItem
+          className={clsx(navigationMenuTriggerStyle())}
+        >
           Post
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Link href='/settings' legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+          <Link href="/settings" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={clsx(
+                navigationMenuTriggerStyle(),
+                activeLinkStyling('/settings')
+              )}
+            >
               Settings
             </NavigationMenuLink>
           </Link>
